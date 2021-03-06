a84c1098453a77beaf2526bc9a522784e23c360e
rudineyliemes <62783403+rudineyliemes@users.noreply.github.com>
2021-03-06 06:42:13 -0300

    convert class in function

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,32 +1,23 @@
-import react, { Component } from 'react';
+import react, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props){
-    super(props);
+const App = () => {
+  const [showImage, setShowImage] = useState(false);
 
-    this.state = {
-      showImage : false,
-    };
-  }
-
-  toggle = () => {
-    this.setState({
-      showImage: !this.state.showImage,
-    });
+  const toggle = () => {
+    setShowImage(!showImage);
   } 
-  
-  render(){ 
+     
     return (
       <div className="App">
         <header className="App-header">
-          <button type="button" onClick={this.toggle}>
+          <button type="button" onClick={toggle}>
 
-          {this.state.showImage ? 'Esconder' : 'Mostrar'}  
+          {showImage ? 'Esconder' : 'Mostrar'}  
 
           </button> 
-          {this.state.showImage && (
+          {showImage && (
             <img src={logo} className="App-logo" alt="logo" />
           )}
           <p>
@@ -43,7 +34,7 @@ class App extends Component {
         </header>
       </div>
     );    
-  }
-}
+  
 
+}
 export default App;
